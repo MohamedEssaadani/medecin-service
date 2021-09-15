1b0e6436ec2578fe38846e9b13f90bff10d55637
Mohamed Es-saadani <essaadani80@gmail.com>
2021-09-15 22:10:54 +0100

    'commit'

---
+++ src/test/java/org/essaadani/medecinservice/MedecinServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.essaadani.medecinservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedecinServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
